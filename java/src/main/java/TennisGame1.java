44c6bbd706d26d500ea9784276b294c83494ce98
Davide Cremonesi <dvd.crm@gmail.com>
2016-06-30 21:54:45 +0200

    Magic strings in constants

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,7 +1,17 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
+	private static final String LOVE = "Love";
+	private static final String ALL = "All";
+	private static final String FIFTEEN = "Fifteen";
+	private static final String THIRTY = "Thirty";
+	private static final String FORTY = "Forty";
+    private static final String DEUCE = "Deuce";
+	private static final String ADVANTAGE = "Advantage ";
+	private static final String WIN_FOR = "Win for ";
+	
+	
+	private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
@@ -40,16 +50,16 @@ public class TennisGame1 implements TennisGame {
             switch (m_score1)
             {
                 case 0:
-                	mStrScore = "Love-All";
+                	mStrScore = LOVE+"-"+ALL;
                     break;
                 case 1:
-                	mStrScore = "Fifteen-All";
+                	mStrScore = FIFTEEN+"-"+ALL;
                     break;
                 case 2:
-                	mStrScore = "Thirty-All";
+                	mStrScore = THIRTY+"-"+ALL;
                     break;
                 default:
-                	mStrScore = "Deuce";
+                	mStrScore = DEUCE;
                     break;
                 
             }
@@ -57,10 +67,10 @@ public class TennisGame1 implements TennisGame {
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) mStrScore ="Advantage player1";
-            else if (minusResult ==-1) mStrScore ="Advantage player2";
-            else if (minusResult>=2) mStrScore = "Win for player1";
-            else mStrScore ="Win for player2";
+            if (minusResult==1) mStrScore =ADVANTAGE + player1Name;
+            else if (minusResult ==-1) mStrScore =ADVANTAGE + player2Name;
+            else if (minusResult>=2) mStrScore = WIN_FOR + player1Name;
+            else mStrScore =WIN_FOR+player2Name;
         }
         else
         {
@@ -71,16 +81,16 @@ public class TennisGame1 implements TennisGame {
                 switch(tempScore)
                 {
                     case 0:
-                    	mStrScore+="Love";
+                    	mStrScore+=LOVE;
                         break;
                     case 1:
-                    	mStrScore+="Fifteen";
+                    	mStrScore+=FIFTEEN;
                         break;
                     case 2:
-                    	mStrScore+="Thirty";
+                    	mStrScore+=THIRTY;
                         break;
                     case 3:
-                    	mStrScore+="Forty";
+                    	mStrScore+=FORTY;
                         break;
                 }
             }
